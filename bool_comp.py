ad690505a430cb286ab8dfb6e414cd681c78bff7
Frank <f.r.harraway@student.tudelft.nl>
2024-04-04 17:35:58 +0200

    added heatmaps

--- bool_comp.py
+++ bool_comp.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import nnls
 import cvxpy as cp
+import seaborn as sns
 
 np.random.seed(0)
 
@@ -62,7 +63,7 @@ def nnomp(tolerance, y, A):
         
     return x_hat, S
 
-def basis_pursuit(y, A):
+def basis_pursuit(y, A,c):
     # n is the number of variables in the signal x we want to recover
     n = A.shape[1]
     
@@ -73,13 +74,13 @@ def basis_pursuit(y, A):
     objective = cp.Minimize(cp.norm(x, 1))
     
     # Define the constraints (Ax = y)
-    constraints = [A @ x == y,x>=0,x<=1]
+    constraints = [cp.norm(A @ x - y, 2) <= c, x >= 0, x <= 1]
     
     # Define the problem
     problem = cp.Problem(objective, constraints)
     
     # Solve the problem
-    problem.solve(solver=cp.CVXOPT)
+    problem.solve(solver=cp.SCS,verbose=False)
     
     if x.value is not None:  # Check if the solution exists
         x_hat = x.value
@@ -89,9 +90,9 @@ def basis_pursuit(y, A):
         x_hat = np.where(x_hat >= threshold, 1, x_hat)  # This sets values above or equal to threshold to 1
     else:
         print("Solution not found or problem is infeasible.")
+        # print(y)
         x_hat = np.zeros(n)
     return x_hat
-
 
 def hamming_distance(array1, array2):
     """
@@ -115,32 +116,77 @@ def hamming_distance(array1, array2):
 
 
 # Parameters
-M = 1000 #Number of measurements
+
 N = 100  # Size of each measurement
-p = 0.0007  # Mean of the Bernoulli distribution
 #Load the data (the N*1 sparse vector)
 # ith value of 0: not infected and 1: infected
-#group_data M samples of size 100 soldiers
-group_data=loadmat('/home/frank/Documents/git/Boolean_compressed_Sensing/GroupTesting.mat')['x'][:,:N]
-total_hamming_omp=0
-total_hamming_basis=0
+#group_data M samples of size 100 patients
+group_data=loadmat('/home/frank/Documents/git/Boolean_compressed_Sensing/GroupTesting.mat')['x'][1:200,:N]
+
+# Define the range of values for p and M
+p_values =np.array([0.001,0.0025,0.005,0.01,0.05,0.1,0.2,0.4])
+M_values = np.arange(10, 211, 20)
+
+# Initialize an empty dictionary to store the hamming distances
+hamming_distances_basis = { (p, M): 0 for p in p_values for M in M_values}
+hamming_distances_omp = { (p, M): 0 for p in p_values for M in M_values}
+
+
+
+# Create a list of p and M values
+p_values = sorted(list(set([key[0] for key in hamming_distances_omp.keys()])))
+M_values = sorted(list(set([key[1] for key in hamming_distances_omp.keys()])))
+
+
+# Iterate over p and M values
+for p in p_values:
+    for M in M_values:
+        print("p: ",p,"\t M:", M)
+        # Initialize the total hamming distance for basis pursuit
+        total_hamming_omp = 0
+        total_hamming_basis = 0
+        
+        # Iterate over group_data
+        for i, s in enumerate(group_data):
+            Y, A = construct_measurements_randompool(s, M, p, N)
+            z_omp = nnomp(10**-40,Y,A)[0]
+            try:
+                z_basis = basis_pursuit(Y, A, 1)
+            except:
+                print("failed")
+                z_basis=np.zeros(N)
+            z_omp = np.ceil(z_omp)
+            total_hamming_omp += hamming_distance(s, z_omp)
+            total_hamming_basis += hamming_distance(s, z_basis)
+        
+        # Store the total hamming distance for this p and M combination
+        hamming_distances_omp[(p, M)] = total_hamming_omp
+        hamming_distances_basis[(p, M)] = total_hamming_basis
+
+
+# Create a matrix of hamming distances
+hamming_matrix_omp = np.zeros((len(p_values), len(M_values)))
+hamming_matrix_basis = np.zeros((len(p_values), len(M_values)))
+for i, p in enumerate(p_values):
+    for j, M in enumerate(M_values):
+        hamming_matrix_omp[i, j] = hamming_distances_omp[(p, M)]
+        hamming_matrix_basis[i, j] = hamming_distances_basis[(p, M)]
 
 
 
 
-for i,s in enumerate(group_data):
+#Plot the heat map
+sns.heatmap(hamming_matrix_omp, xticklabels=M_values, yticklabels=p_values)
+plt.xlabel('M (Number of Measurements)')
+plt.ylabel('p (Probability)')
+plt.title('Total Hamming Distance for NNOMP')
+plt.show()
 
-    Y,A=construct_measurements_randompool(s, M, p,N)
 
-    z_basis=basis_pursuit(Y,A)
-    z_omp,S=nnomp(10**-40,Y,A)
-    z_omp=np.ceil(z_omp)
-    # print("Group_data:",np.nonzero(s))
-    # print("z:",np.sort(list(S)))
-    # print("Hamming distance basis:", hamming_distance(s, z_basis), "\ttotal non zero:",len(np.nonzero(s)[0]))
-    # print("Hamming distance:", hamming_distance(s, z), "\ttotal non zero:",len(np.nonzero(s)[0]))
-    total_hamming_omp+=hamming_distance(s, z_omp)
-    total_hamming_basis+=hamming_distance(s, z_basis)
-    
-print("Total hamming distance:",total_hamming_basis)
-print("Total hamming distance:",total_hamming_omp)
+# Plot the heat map
+print(hamming_distances_basis)
+sns.heatmap(hamming_matrix_basis, xticklabels=M_values, yticklabels=p_values)
+plt.xlabel('M (Number of Measurements)')
+plt.ylabel('p (Probability)')
+plt.title('Total Hamming Distance for basis pursuit')
+plt.show()
